2f2f29332f549520f358548a9b1a7ac792be9933
Eshwari Bhide <ebhide99@gmail.com>
2022-01-19 09:49:10 -0500

    Add button and toast

--- app/src/main/java/edu/neu/madcourse/numad22sp_eshwaribhide/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_eshwaribhide/MainActivity.java
@@ -3,6 +3,8 @@ package edu.neu.madcourse.numad22sp_eshwaribhide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(String toastMsg) {
+        Toast toast = Toast.makeText(this, toastMsg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        showToast("Eshwari Bhide, bhide.e@northeastern.edu");
     }
 }
