643b98a7de987a163fb294da25d222a4166e5a72
Eshwari Bhide <ebhide99@gmail.com>
2022-01-28 16:18:05 -0500

    Removed toast and instead display information in new screen

--- app/src/main/java/edu/neu/madcourse/numad22sp_eshwaribhide/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_eshwaribhide/MainActivity.java
@@ -2,24 +2,33 @@ package edu.neu.madcourse.numad22sp_eshwaribhide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "edu.neu.madcourse.numad22sp_eshwaribhide.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void showToast(String toastMsg) {
-        Toast toast = Toast.makeText(this, toastMsg, Toast.LENGTH_LONG);
-        toast.show();
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String msg = "Eshwari Bhide, bhide.e@northeastern.edu";
+        intent.putExtra(EXTRA_MESSAGE, msg);
+        startActivity(intent);
     }
 
-    public void displayToastMsg(View v) {
-        showToast("Eshwari Bhide, bhide.e@northeastern.edu");
-    }
+//    public void showToast(String toastMsg) {
+//        Toast toast = Toast.makeText(this, toastMsg, Toast.LENGTH_LONG);
+//        toast.show();
+//    }
+//
+//    public void displayToastMsg(View v) {
+//        showToast("Eshwari Bhide, bhide.e@northeastern.edu");
+//    }
 }
