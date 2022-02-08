af30a11fbda8e3d3f5bb48e626685371aa31196d
Eshwari Bhide <ebhide99@gmail.com>
2022-02-08 16:57:20 -0500

    Refactoring

--- app/src/main/java/edu/neu/madcourse/numad22sp_eshwaribhide/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_eshwaribhide/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "edu.neu.madcourse.numad22sp_eshwaribhide.MESSAGE";
+    public static final String PKG_NAME = "edu.neu.madcourse.numad22sp_eshwaribhide.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     /** Called when the user taps the About Me button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        String msg = "Eshwari Bhide, bhide.e@northeastern.edu";
-        intent.putExtra(EXTRA_MESSAGE, msg);
+    public void aboutMeOnClick(View view) {
+        Intent intent = new Intent(this, AboutMeActivity.class);
+        String name = "Eshwari Bhide, bhide.e@northeastern.edu";
+        intent.putExtra(PKG_NAME, name);
         startActivity(intent);
     }
 }
